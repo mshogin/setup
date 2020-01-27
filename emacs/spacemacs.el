18fe41488216413b57ba8833a9db11f9e91c1525
Mikhail Shogin <mshogin@gmail.com>
2020-01-27 19:23:38 +0100

    spacemacs config

---
+++ emacs/spacemacs.el
@@ -0,0 +1,41 @@
+dotspacemacs-configuration-layers
+'(nginx
+  python
+  javascript
+  yaml
+  html
+  ;; ----------------------------------------------------------------
+  ;; Example of useful layers you may want to use right away.
+  ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
+  ;; `M-m f e R' (Emacs style) to install them.
+  ;; ----------------------------------------------------------------
+  ;; auto-completion
+  ;; better-defaults
+  emacs-lisp
+  git
+  helm
+  lsp
+  ;; markdown
+  multiple-cursors
+  org
+  ;; (shell :variables
+  ;;        shell-default-height 30
+  ;;        shell-default-position 'bottom)
+  spell-checking
+  syntax-checking
+  treemacs
+  go
+  ;; version-control
+  ;; mu4e
+  )
+
+dotspacemacs-additional-packages '(
+                                   go-dlv
+                                   protobuf-mode
+                                   go-imports
+                                   gotest
+                                   dockerfile-mode
+                                   direnv
+                                   yasnippet-snippets
+                                   groovy-mode
+                                   )
